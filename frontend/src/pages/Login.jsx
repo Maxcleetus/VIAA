b5d3f0a4d5bbf021d1eebcfd6b68e56cfbc299e9
Rino Ringle <ringlerino67@gmail.com>
2025-07-19 14:45:10 +0530

    login complted

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -63,7 +63,7 @@ const Login = () => {
             <div>
               <button
                 type="submit"
-                className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-150"
+                className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition duration-150"
               >
                 Login
               </button>
